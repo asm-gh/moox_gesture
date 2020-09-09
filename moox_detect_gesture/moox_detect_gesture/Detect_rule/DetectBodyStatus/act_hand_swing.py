1a570aafaf694cbb7cb0ec1bf4c77050deee48fb
DonnaldBelen <69287694+DonnaldBelen@users.noreply.github.com>
2020-09-09 14:25:01 +0900

    Improved act_hand_swing.py
    Majority of the algorithm was modified to enhance detection error handling.
    now outliers are removed from the signal.

--- moox_detect_gesture/moox_detect_gesture/Detect_rule/DetectBodyStatus/act_hand_swing.py
+++ moox_detect_gesture/moox_detect_gesture/Detect_rule/DetectBodyStatus/act_hand_swing.py
@@ -38,12 +38,20 @@ class Act_Hand_Swing:
         self.thresh_small = inifile.getint('gesture_recognition','thresh_wave_small')
         self.thresh_med = inifile.getint('gesture_recognition','thresh_wave_medium')
         self.thresh_large = inifile.getint('gesture_recognition','thresh_wave_large')
-        self.handtip_L_x_recent = deque(maxlen=movement_window) #NCP Edit
-        self.handtip_R_x_recent = deque(maxlen=movement_window) #NCP Edit
+        self.handtip_L_x_recent = deque([0],maxlen=movement_window)
+        self.handtip_R_x_recent = deque([0],maxlen=movement_window)
+        self.handtip_L_z_recent = deque([0],maxlen=movement_window)
+        self.handtip_R_z_recent = deque([0],maxlen=movement_window)
 
         self.is_r_hand_swing = 0
         self.is_l_hand_swing = 0
         self.is_hand_swing = 0
+
+        self.window_move_hilo_R = deque([0],maxlen=movement_window)
+        self.window_move_hilo_L = deque([0],maxlen=movement_window)
+        self.window_move_hilo_R_z = deque([0],maxlen=movement_window)
+        self.window_move_hilo_L_z = deque([0],maxlen=movement_window)
+
 
     def calculate(self,
                   r_wrist=np.zeros(3),
@@ -65,7 +73,9 @@ class Act_Hand_Swing:
         self.is_hand_swing = 0
         self.is_r_hand_swing = 0
         self.is_l_hand_swing = 0
-        
+        self.is_r_hand_swing_z = 0
+        self.is_l_hand_swing_z = 0
+
         is_r_hand_up = False
         is_l_hand_up = False
         is_hand_up = False
@@ -79,9 +89,60 @@ class Act_Hand_Swing:
         thresh_large = self.thresh_large
         boundary_line = 10
 
+        l_handtip_dif = 0
+        r_handtip_dif = 0
+        l_handtip_dif_z = 0
+        r_handtip_dif_z = 0
+
+
+        flag_movebothways_L = False
+        flag_movebothways_R = False
+        flag_movebothways_L_z = False
+        flag_movebothways_R_z = False
+
+
         if (is_data):
-            self.handtip_L_x_recent.append(l_handtip[x_idx])
-            self.handtip_R_x_recent.append(r_handtip[x_idx])
+
+            l_handtip_dif = l_handtip[x_idx] -self.handtip_L_x_recent[-1]
+            r_handtip_dif = r_handtip[x_idx] -self.handtip_R_x_recent[-1]
+            if abs(l_handtip_dif) < 600:
+                self.handtip_L_x_recent.append(l_handtip[x_idx])
+                if l_handtip_dif > 0:
+                    self.window_move_hilo_L.append(1)
+                else:
+                    self.window_move_hilo_L.append(-1)
+            if abs(r_handtip_dif) < 600: #-# Same as left hand
+                self.handtip_R_x_recent.append(r_handtip[x_idx])
+                if r_handtip_dif > 0:
+                    self.window_move_hilo_R.append(1)
+                else:
+                    self.window_move_hilo_R.append(-1)
+
+            if abs(np.sum(self.window_move_hilo_L)) < 3:
+                flag_movebothways_L = True
+            if abs(np.sum(self.window_move_hilo_R)) < 3:
+                flag_movebothways_R = True
+
+
+            l_handtip_dif_z = l_handtip[z_idx] -self.handtip_L_z_recent[-1]
+            r_handtip_dif_z = r_handtip[z_idx] -self.handtip_R_z_recent[-1]
+            if abs(l_handtip_dif_z) < 600:
+                self.handtip_L_z_recent.append(l_handtip[z_idx])
+                if l_handtip_dif_z > 0:
+                    self.window_move_hilo_L_z.append(1)
+                else:
+                    self.window_move_hilo_L_z.append(-1)
+            if abs(r_handtip_dif) < 600:
+                self.handtip_R_z_recent.append(r_handtip[z_idx])
+                if r_handtip_dif > 0:
+                    self.window_move_hilo_R_z.append(1)
+                else:
+                    self.window_move_hilo_R_z.append(-1)
+
+            if abs(np.sum(self.window_move_hilo_L_z)) < 3:
+                flag_movebothways_L_z = True
+            if abs(np.sum(self.window_move_hilo_R_z)) < 3:
+                flag_movebothways_R_z = True
 
             self.is_hand_swing = 0
             self.is_l_hand_swing = 0
@@ -90,29 +151,59 @@ class Act_Hand_Swing:
             move_amnt_R = np.percentile(self.handtip_R_x_recent,90) - np.percentile(self.handtip_R_x_recent,10)
             move_amnt_L = np.percentile(self.handtip_L_x_recent,90) - np.percentile(self.handtip_L_x_recent,10)
 
+            if flag_movebothways_R:
+                if (move_amnt_R) > thresh_small:
+                    if r_wrist[y_idx] > r_elbow[y_idx]:
+                        if (r_wrist[y_idx] > naval[y_idx]):
+                            is_r_hand_up = True
+                            self.is_r_hand_swing = 1
+                            if (move_amnt_R) > thresh_large:
+                                self.is_r_hand_swing = 3
+                            elif (move_amnt_R) > thresh_med:
+                                self.is_r_hand_swing = 2
+            if flag_movebothways_L: #NCP
+                if (move_amnt_L) > thresh_small:
+                    if l_wrist[y_idx] > l_elbow[y_idx]:
+                        if (l_wrist[y_idx] > naval[y_idx]):
+                            is_l_hand_up = True
+                            self.is_l_hand_swing = 1
+                            if (move_amnt_L) > thresh_large:
+                                self.is_l_hand_swing = 3
+                            elif (move_amnt_L) > thresh_med:
+                                self.is_l_hand_swing = 2
 
-            if (move_amnt_R) > thresh_small:
-                if r_wrist[y_idx] > r_elbow[y_idx]:
-                    if (r_wrist[y_idx] > naval[y_idx]):
-                        is_r_hand_up = True
-                        self.is_r_hand_swing = 1
-                        if (move_amnt_R) > thresh_large:
-                            self.is_r_hand_swing = 3
-                        elif (move_amnt_R) > thresh_med:
-                            self.is_r_hand_swing = 2
-            if (move_amnt_L) > thresh_small:
-                if l_wrist[y_idx] > l_elbow[y_idx]:
-                    if (l_wrist[y_idx] > naval[y_idx]):
-                        is_l_hand_up = True
-                        self.is_l_hand_swing = 1
-                        if (move_amnt_L) > thresh_large:
-                            self.is_l_hand_swing = 3
-                        elif (move_amnt_L) > thresh_med:
-                            self.is_l_hand_swing = 2
-                
+## Z Block 2 - Start
+            move_amnt_R_z = np.percentile(self.handtip_R_z_recent,90) - np.percentile(self.handtip_R_z_recent,10)
+            move_amnt_L_z = np.percentile(self.handtip_L_z_recent,90) - np.percentile(self.handtip_L_z_recent,10)
+
+            if flag_movebothways_R_z:
+                if (move_amnt_R) > thresh_small:
+                    if r_wrist[y_idx] > r_elbow[y_idx]:
+                        if (r_wrist[y_idx] > naval[y_idx]):
+                            is_r_hand_up = True
+                            self.is_r_hand_swing_z = 1
+                            if (move_amnt_R) > thresh_large:
+                                self.is_r_hand_swing_z = 3
+                            elif (move_amnt_R) > thresh_med:
+                                self.is_r_hand_swing_z = 2
+            if flag_movebothways_L_z:
+                if (move_amnt_L_z) > thresh_small:
+                    if l_wrist[y_idx] > l_elbow[y_idx]:
+                        if (l_wrist[y_idx] > naval[y_idx]):
+                            is_l_hand_up = True
+                            self.is_l_hand_swing_z = 1
+                            if (move_amnt_L_z) > thresh_large:
+                                self.is_l_hand_swing_z = 3
+                            elif (move_amnt_L_z) > thresh_med:
+                                self.is_l_hand_swing_z = 2
+
+## Z Block 2 - End
+
             swing_val = []
             swing_val.append(self.is_r_hand_swing)
             swing_val.append(self.is_l_hand_swing)
+            swing_val.append(self.is_r_hand_swing_z)
+            swing_val.append(self.is_l_hand_swing_z)
             self.is_hand_swing = max(swing_val)
 
         return self.is_hand_swing, self.is_r_hand_swing, self.is_l_hand_swing
