81aa17d45da13dde81719a8ea183ad655470ac19
DonnaldBelen <69287694+DonnaldBelen@users.noreply.github.com>
2020-09-11 15:43:01 +0900

    Update act_hand_swipe.py
    get_data function is a summary of the outlier removal code from Nick's concept.
    all data will now be validated before being processed.
    
    all computations are placed inside the conditional if statement to minimize math process if condition is not met. this is to improve
    detection speed.

--- moox_detect_gesture/moox_detect_gesture/Detect_rule/DetectBodyStatus/act_hand_swipe.py
+++ moox_detect_gesture/moox_detect_gesture/Detect_rule/DetectBodyStatus/act_hand_swipe.py
@@ -40,6 +40,10 @@ class Act_Hand_Swipe:
         self.thresh_large = inifile.getint('gesture_recognition','thresh_wave_large')
         self.handtip_L_x_recent = deque(maxlen=movement_window)
         self.handtip_R_x_recent = deque(maxlen=movement_window)
+        self.handtip_L_z_recent = deque(maxlen=movement_window)
+        self.handtip_R_z_recent = deque(maxlen=movement_window)
+        self.handtip_recent = deque(maxlen=movement_window)
+        self.window_move = deque([0],maxlen=movement_window)
 
         self.is_r_hand_swipe = 0
         self.is_l_hand_swipe = 0
@@ -79,29 +83,36 @@ class Act_Hand_Swipe:
         if (is_data):
             self.handtip_L_x_recent.append(l_handtip[x_idx])
             self.handtip_R_x_recent.append(r_handtip[x_idx])
+            self.handtip_L_z_recent.append(l_handtip[x_idx])
+            self.handtip_R_z_recent.append(r_handtip[x_idx])
 
             self.is_hand_swipe = 0
             self.is_l_hand_swipe = 0
             self.is_r_hand_swipe = 0
 
+            if r_wrist[y_idx] > r_elbow[y_idx]:
+                if (r_wrist[x_idx] > boundary_line):
+                    if (r_wrist[y_idx] > naval[y_idx]):
+                        move_amnt_R = np.percentile(self.handtip_R_x_recent,90) - np.percentile(self.handtip_R_x_recent,10)
+                        move_amnt_R_z = np.percentile(self.handtip_R_z_recent,90) - np.percentile(self.handtip_R_z_recent,10)
+                        r_handtip_dif = r_handtip[x_idx] - self.handtip_R_x_recent[-1]
+                        r_handtip_dif_z = r_handtip[z_idx] - self.handtip_R_z_recent[-1]
+                        handtip_dif = r_handtip_dif if r_handtip_dif > r_handtip_dif_z else r_handtip_dif_z
+                        r_hand_tip_x = r_handtip[x_idx] if r_handtip_dif > r_handtip_dif_z else r_handtip[z_idx]
+                        thresh_amnt_R = move_amnt_R if move_amnt_R > move_amnt_R_z else move_amnt_R_z
+                        self.is_r_hand_swipe = 1 if self.get_data(handtip_dif, r_hand_tip_x, thresh_amnt_R) else 0
 
-            move_amnt_R = np.percentile(self.handtip_R_x_recent,90) - np.percentile(self.handtip_R_x_recent,10)
-            move_amnt_L = np.percentile(self.handtip_L_x_recent,90) - np.percentile(self.handtip_L_x_recent,10)
-
-
-            if (move_amnt_R) > thresh_small:
-                if r_wrist[y_idx] > r_elbow[y_idx]:
-                    if (r_wrist[x_idx] > boundary_line):
-                        if (r_wrist[y_idx] > naval[y_idx]):
-                            if (move_amnt_R) > thresh_large:
-                                self.is_r_hand_swipe = 1
-
-            if (move_amnt_L) > thresh_small:
-                if l_wrist[y_idx] > l_elbow[y_idx]:
-                    if (l_wrist[x_idx] > boundary_line):
-                        if (l_wrist[y_idx] > naval[y_idx]):
-                            if (move_amnt_L) > thresh_large:
-                                self.is_l_hand_swipe = 1
+            if l_wrist[y_idx] > l_elbow[y_idx]:
+                if (l_wrist[x_idx] > boundary_line):
+                    if (l_wrist[y_idx] > naval[y_idx]):
+                        move_amnt_L = np.percentile(self.handtip_L_x_recent,90) - np.percentile(self.handtip_L_x_recent,10)
+                        move_amnt_L_z = np.percentile(self.handtip_L_z_recent,90) - np.percentile(self.handtip_L_z_recent,10)
+                        l_handtip_dif = l_handtip[x_idx] - self.handtip_L_x_recent[-1]
+                        l_handtip_dif_z = l_handtip[z_idx] - self.handtip_L_z_recent[-1]
+                        handtip_dif =  l_handtip_dif if l_handtip_dif > l_handtip_dif_z else l_handtip_dif_z
+                        l_hand_tip_x = l_handtip[x_idx] if l_handtip_dif > l_handtip_dif_z else l_handtip[z_idx]
+                        thresh_amnt_L = move_amnt_L if move_amnt_L > move_amnt_L_z else move_amnt_L_z
+                        self.is_l_hand_swipe = 1 if self.get_data(handtip_dif, l_hand_tip_x , thresh_amnt_L) else 0
                 
             swipe_val = []
             swipe_val.append(self.is_r_hand_swipe)
@@ -109,3 +120,21 @@ class Act_Hand_Swipe:
             self.is_hand_swipe = max(swipe_val)
 
         return self.is_hand_swipe, self.is_r_hand_swipe, self.is_l_hand_swipe
+
+    def get_data(self, handtip_dif, handtip, move_amnt):
+        thresh_small = self.thresh_small
+        flag_movebothways = False
+        if abs(handtip_dif) < 600:
+            self.handtip_recent.append(handtip)
+            if handtip_dif > 0:
+                self.window_move.append(1)
+            else:
+                self.window_move.append(-1)
+        if abs(np.sum(self.window_move)) < 3:
+            flag_movebothways = True
+
+        if flag_movebothways == False:
+            if (move_amnt) > thresh_small:
+                return True
+            else:
+                return False
